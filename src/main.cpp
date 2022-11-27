7382b60305fdbbe997877c8724769e3582712b19
Vilik <105110778+VilikNakhshikyan@users.noreply.github.com>
2022-11-27 23:14:32 +0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,6 @@
 #include <string>
 #include "include/my_arr.h"
 #include <vector>
-// bool isuniq(const std::string &txt)
-// {
-//     unsigned int binar_key[8]{0};
-//     short index;
-//     for (char ch : txt)
-//     {
-//         index = ch / 32;
-//         ch %= 32;
-//         if (binar_key[index] & (1 << ch))
-//             return false;
-//         else
-//             binar_key[index] |= (1 << ch);
-//     }
-//     return true;
-// }
 
 int main()
 {
